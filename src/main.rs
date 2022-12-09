e13d429e74c9092269cf25966eea45682d8eb3a3
martin <csaknorrisz@gmail.com>
2022-12-09 09:50:57 +0100

    mcts

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ use std::process::exit;
 use policies::{Policy, Connect4Net, NNPolicy};
 use connect4::{Connect4, Game};
 use base65536::{load_1d, load_2d, f16_to_f32, f32_to_bf16};
+use mcts::MCTS;
 use weights::*;
+use config::*;
+
 mod connect4;
 mod policies;
 mod nn;
 mod weights;
 mod base65536;
-
+mod mcts;
+mod config;
 
 
 fn main() {
@@ -33,4 +37,25 @@ fn main() {
     for ff in a {
         println!("{}", ff);
     }
+
+    let mcts_cfg = MCTSConfig {
+        exploration: Exploration::PolynomialUct { c: 3.0 }, // type of exploration to use (e.g. PUCT or UCT)
+        solve: true,                   // use MCTS Solver extension to solve nodes
+        correct_values_on_solve: true, // if node is solved, adjust previously backprop'd values
+        select_solved_nodes: true,     // select nodes that are solved
+        auto_extend: true,             // visit nodes until a node with > 1 child is reached
+        fpu: Fpu::ParentQ,
+        root_policy_noise: PolicyNoise::None,
+    };
+
+    let mut mcts =
+            MCTS::with_capacity(2000, mcts_cfg, &mut policy, c4.clone());
+
+        // explore
+        mcts.explore_n(1000);
+
+
+    let a = mcts.best_action(ActionSelection::NumVisits);
+    let b: usize = a.into();
+    print!("{}", b);
 }
